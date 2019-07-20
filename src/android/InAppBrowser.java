75a2d19ced908636a4129c5cc037a32d4414f7be
Saurabh Gangamwar <saurabh.gangamwar@demandfarm.onmicrosoft.com>
2019-07-21 02:25:50 +0530

    added cookies parameter along with 'url' and 'type' loadStart event callback in android

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1398,6 +1398,8 @@ public class InAppBrowser extends CordovaPlugin {
 
             try {
                 JSONObject obj = new JSONObject();
+                String cookies = CookieManager.getInstance().getCookie(url);
+                obj.put("cookies", cookies);
                 obj.put("type", LOAD_START_EVENT);
                 obj.put("url", newloc);
                 sendUpdate(obj, true);
